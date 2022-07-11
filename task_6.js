8083efcba0e4d6ede0d9e1f56175857d8cdfb40d
Siesta <siesta.cloud@gmail.com>
2022-07-11 08:58:12 +0300

    ok

--- task_6.js
+++ task_6.js
@@ -1,16 +1,18 @@
 // Task 6. 10.6 Циклы
 console.log("Task 6. 10.6 Циклы.");
 let array = ["ok", "ok", "k"]
+let array2 = ["1", 1, 1]
+
 let baseEl;
 function checkEqual(array) {
     for (let index = 0; index < array.length; index++) {
         if (index == 0) {
             baseEl = array[index];
         }
-        if (baseEl != array[index]) {
+        if (baseEl !== array[index]) {
             return false
         }
     }
     return true
 }
-console.log(checkEqual(array));
+console.log(checkEqual(array2));
